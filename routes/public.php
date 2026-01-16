396576d39c23c14ca4dff17072504471e8dbc57a
fardin ahsan <fardin.ahsan75@gmail.com>
2026-01-17 04:26:44 +0600

    first commit

--- routes/public.php
+++ routes/public.php
@@ -1,36 +1,6 @@
 <?php
-
-use App\Http\Controllers\Public\AboutController;
-use App\Http\Controllers\Public\BlogController;
-use App\Http\Controllers\Public\CaseStudyController;
-use App\Http\Controllers\Public\ContactController;
-use App\Http\Controllers\Public\FAQController;
 use App\Http\Controllers\Public\HomeController;
-use App\Http\Controllers\Public\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 // Homepage
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-// About
-Route::get('/about', [AboutController::class, 'index'])->name('about');
-
-// Services
-Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
-Route::get('/services/{service:slug}', [ServiceController::class, 'show'])->name('services.show');
-Route::get('/services/{parent:slug}/{child:slug}', [ServiceController::class, 'showChild'])->name('services.child');
-
-// Contact
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
-// Blog
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/blog/{post:slug}', [BlogController::class, 'show'])->name('blog.show');
-
-// Case Studies
-Route::get('/case-studies', [CaseStudyController::class, 'index'])->name('case-studies.index');
-Route::get('/case-studies/{caseStudy:slug}', [CaseStudyController::class, 'show'])->name('case-studies.show');
-
-// FAQs
-Route::get('/faq', [FAQController::class, 'index'])->name('faq.index');
